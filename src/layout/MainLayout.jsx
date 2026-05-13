11c9121a1472d1bf769b49e526acc97ddcdc1035
saima66 <saimasultana6112015@gmail.com>
2026-05-13 21:29:58 +0600

    Add footer

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Outlet } from 'react-router';
 import Navbar from '../Components/Navbar';
+import Footer from '../Components/Footer';
 
 const MainLayout = () => {
     return (
@@ -12,7 +13,7 @@ const MainLayout = () => {
                 <Outlet></Outlet>
             </main>
             <footer className='w-11/12 mx-auto'>
-                {/* <Footer></Footer> */}
+                <Footer></Footer>
             </footer>
         </div>
     );
